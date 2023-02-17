4a2d2b3b4c25219af719003438d7e516bbb50f1e
Polar Bear <xadjimurodaliyev@gmail.com>
2023-02-17 21:31:53 +0500

    Make a header

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express")
+const uploadImages = require("./uploadImages.js")
 const app = express()
 const bodyParser = require("body-parser")
 app.use(express.static("."))
 app.set("view engine", "ejs")
 
+
+
 app.get("/", function(req, res){
+	uploadImages.save()
 	res.render("index", {page: "NFT market shop"})
+})
+
+app.post("/", function(req, res){
+	res.redirect("/")
 })
 
 
--- uploadImages.js
+++ uploadImages.js
@@ -1,40 +1,43 @@
-const config = require("./config.js")
-const https = require("https");
-const sys = require("fs")
-const imageDownloader = require("image-downloader")
-let jsonData = []
+function save(){
+	const config = require("./config.js")
+	const https = require("https");
+	const imageDownloader = require("image-downloader")
+	let jsonData = []
 
 
-const options = {
-	hostname: config.name,
-	path: config.url,
-	headers: {"X-Figma-Token": config.token}
+	const options = {
+		hostname: config.name,
+		path: config.url,
+		headers: {"X-Figma-Token": config.token}
 
-};
+	};
 
-function saveImage(){
-	const savedUrl = jsonData.meta.images
-	let count = 0;
-	for(let imgUrl in savedUrl){
-		count++;
-		const options = {
-			url: savedUrl[imgUrl],
-			dest: `D:/NFT/public/images/photo_${count}.png`,
-		};
+	function saveImage(){
+		const savedUrl = jsonData.meta.images
+		let count = 0;
+		for(let imgUrl in savedUrl){
+			count++;
+			const options = {
+				url: savedUrl[imgUrl],
+				dest: `D:/NFT/public/images/photo_${count}.png`,
+			};
 
-		imageDownloader.image(options)
-		.then(({ filename }) => {
-			console.log('Saved to', filename);
+			imageDownloader.image(options)
+			.then(({ filename }) => {
+				console.log('Saved to', filename);
+			})
+			.catch((err) => console.error(err));
+		}	
+	}
+
+	https.get(options, function(res){
+		res.on("data", (data)=>{
+			jsonData.push(data)
+		}).on("end", function(){
+			jsonData = JSON.parse(jsonData)
+			saveImage()
 		})
-		.catch((err) => console.error(err));
-	}	
+	})
 }
 
-https.get(options, function(res){
-	res.on("data", (data)=>{
-		jsonData.push(data)
-	}).on("end", function(){
-		jsonData = JSON.parse(jsonData)
-		saveImage()
-	})
-})
+exports.save = save
